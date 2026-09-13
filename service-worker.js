98be2c1c557ff6212745f8dd955dd43ac9dfce2a
ilanshahaf-droid <ilanshahaf@gmail.com>
2026-09-13 16:09:38 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,9 @@ const CACHE_NAME = "lean-yotsim-cache-v1";
 const ASSETS = [
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/maskable-icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png",
+  "./maskable-icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
